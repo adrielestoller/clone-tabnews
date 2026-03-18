af56e6286eb93b63488d89e2cc24a145c222cad0
Adriel Estoller <adrielestoller@gmail.com>
2026-03-18 00:36:48 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Epilif tá aqui pra ajudar no curso.dev e eu to aqui pra fazer o curso :)</h1>
+  return (
+    <h1>
+      {" "}
+      Epilif tá aqui pra ajudar no curso.dev e eu to aqui pra fazer o curso :)
+    </h1>
+  );
 }
 
 export default Home;
